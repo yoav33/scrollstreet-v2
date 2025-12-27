91762d86a5e57f26c88bbd38bd82d28f2c2ebf40
yoav33 <yoav.grinberg1@gmail.com>
2025-12-27 16:31:22 +0100

    Updates

--- _next/static/vZ8Z7GcRjHrkV666Gy_RO/_buildManifest.js
+++ _next/static/vZ8Z7GcRjHrkV666Gy_RO/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/scrollstreet-v2//_next/:path+",
+        "destination": "/scrollstreet-v2/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
